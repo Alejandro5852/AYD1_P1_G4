27870060b5ad58319e1fed212ea0947d5740db36
Diego Alejandro Martinez Garcia <3042499630114@ingenieria.usac.edu.gt>
2023-02-18 14:18:59 -0600

    Front actualizado
    Se agregaron los siguientes campos en el front: nombre, appelido, telefono y correo.
    Validaciones con react-hook

--- FrontEnd/practica1/src/Nuevo-Contacto/NuevoContacto.js
+++ FrontEnd/practica1/src/Nuevo-Contacto/NuevoContacto.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useForm} from 'react-hook-form'
 
 const NuevoContacto = () => {
+  const { register, handleSubmit, formState: {errors}} = useForm()
+  const customSubmit = (data) => {
+    //console.log(data)
+    alert("¡Validacion exitosa!")
+  }
+
   return (
-    <div>NuevoContacto</div>
+    <>
+      <h2>Registro de contactos</h2>
+
+      <form onSubmit={handleSubmit(customSubmit)} className='form-react'>
+          <div className='form-control'>
+              <label>Nombre</label>
+              <input type="text" {...register('name', {required:true})} />
+              {errors.name?.type === 'required' && <small className='fail'>El campo no puede estar vacio</small>}
+          </div>
+          <div className='form-control'>
+              <label>Apellido</label>
+              <input type="text" {...register('apellido', {required:true})} />
+              {errors.apellido?.type === 'required' && <small className='fail'>El campo no puede estar vacio</small>}
+          </div>
+          <div className='form-control'>
+              <label>Telefono</label>
+              <input type="number" {...register('telefono', {
+                    required:true
+                })} />
+                {errors.telefono?.type === 'required' && <small className='fail'>El campo no puede estar vacio</small>}
+          </div>
+          <div className='form-control'>
+              <label>Correo</label>
+              <input type="text" {...register('correo', {
+                    required:true,
+                    pattern: /\S+@\S+\.\S+/
+              })} />
+              {errors.correo?.type === 'required' && <small className='fail'>El campo no puede estar vacio</small>}
+              {errors.correo?.type === 'pattern' && <small className='fail'>Correo electronico no valido</small>}
+          </div>
+          <button type='submit'>Agregar</button>
+      </form>
+    
+    </>
   )
 }
 
